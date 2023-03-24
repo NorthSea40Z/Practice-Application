ceb22be0a372ee8945bde41f91a215e53b745c63
NorthSea40Z <northsea40.z@gmail.com>
2023-03-24 18:53:16 +0800

    Fix gradle issues to build and run default (example) AndroidTest

--- app/src/main/java/org/northsea40/practiceapplication/Model/AppDatabase.kt
+++ app/src/main/java/org/northsea40/practiceapplication/Model/AppDatabase.kt
@@ -3,7 +3,8 @@ package org.northsea40.practiceapplication.Model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1, exportSchema = false)
+    // Schema export directory is not provided to the annotation processor so we cannot export the schema. You can either provide `room.schemaLocation` annotation processor argument OR set exportSchema to false.
 abstract class AppDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
 }
