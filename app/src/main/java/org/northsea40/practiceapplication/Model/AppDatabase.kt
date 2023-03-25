df6754cf0b74f5b86c3e24176452aed722d5bb8b
NorthSea40Z <northsea40.z@gmail.com>
2023-03-25 14:29:55 +0800

    Split ROOM DAO into interface and implementation(s)
    Also, a ROOM Database class is 1-1 mapping to a DAO implementation

--- app/src/main/java/org/northsea40/practiceapplication/Model/AppDatabase.kt
+++ app/src/main/java/org/northsea40/practiceapplication/Model/AppDatabase.kt
@@ -6,5 +6,5 @@ import androidx.room.RoomDatabase
 @Database(entities = [User::class], version = 1, exportSchema = false)
     // Schema export directory is not provided to the annotation processor so we cannot export the schema. You can either provide `room.schemaLocation` annotation processor argument OR set exportSchema to false.
 abstract class AppDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
+    abstract fun userDao(): UserDaoImpl
 }
---
+++ app/src/main/java/org/northsea40/practiceapplication/Model/UserDaoImpl.kt
@@ -0,0 +1,27 @@
+package org.northsea40.practiceapplication.Model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDaoImpl: UserDao {
+    @Query("SELECT * FROM user")
+    override fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    override fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND last_name LIKE :last LIMIT 1")
+    override fun findByName(first: String, last: String): User
+
+    @Insert
+    override fun insertAll(vararg users: User)
+
+    @Delete
+    override fun delete(user: User)
+
+    // https://developer.android.com/training/data-storage/room/accessing-data?hl=zh-tw
+    // https://www.w3schools.com/sql/sql_syntax.asp
+}
