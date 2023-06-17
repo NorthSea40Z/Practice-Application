371f1f309441926f17902daee21e9d427118c438
ian.lin <Ian.Lin@idtechproducts.com>
2023-06-17 18:04:45 +0800

    Material Design

--- app/src/main/java/org/northsea40/practiceapplication/MainActivity.kt
+++ app/src/main/java/org/northsea40/practiceapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package org.northsea40.practiceapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
@@ -50,19 +52,34 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)  // step 11
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)    // stop M03
         )   // step 9
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp)) // step 12
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleMedium    // step M04
+            )
 
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))    // step 13
 
             Text(text = msg.body)
         }   // step 7
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Surface(shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp) {
+            Text(
+                text = msg.body,
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }   // step M05
     }
 }
 
@@ -84,3 +101,20 @@ fun MessageCard(msg: Message) {
 //        Greeting("Android")
 //    }
 //}   // step 3a (This is only for preview)
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)   // step M06
+@Composable
+fun PreviewMessageCard() {
+    PracticeApplicationTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }   // step M02
+}
